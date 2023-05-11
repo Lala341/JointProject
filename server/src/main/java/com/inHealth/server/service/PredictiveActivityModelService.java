430298c49a2cc08b83178565f8c727e2c6f52640
lala341 <li.forero@uniandes.edu.co>
2023-05-11 03:14:33 -0500

    Changes data

--- server/src/main/java/com/inHealth/server/service/PredictiveActivityModelService.java
+++ server/src/main/java/com/inHealth/server/service/PredictiveActivityModelService.java
@@ -12,12 +12,16 @@ import org.springframework.boot.SpringApplication;
 public class PredictiveActivityModelService {
 
     static public  void createModel() {
-        SparkConf conf = new SparkConf().setAppName("App Name").setMaster("spark://localhost:7077");
+        System.setProperty("HADOOP_CONF_DIR",  "/sensors-data");
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+        SparkConf conf = new SparkConf().setAppName("InHealthSensors").setMaster("spark://localhost:7077");
         System.out.println(conf);
         JavaSparkContext sc = new JavaSparkContext(conf);
         System.out.println(sc);
 
-        JavaRDD<String> data = sc.textFile("hdfs://namenode:9000/sensors-data/1e9c1862-ec25-4cde-a689-38ab696ccbc0/sensor-data_1e9c1862-ec25-4cde-a689-38ab696ccbc0_2023-04-19T13-41-44.txt");
+
+        JavaRDD<String> data = sc.textFile("hdfs://localhost:9000/sensors-data/1e9c1862-ec25-4cde-a689-38ab696ccbc0/sensor-data_1e9c1862-ec25-4cde-a689-38ab696ccbc0_2023-04-19T13-41-44.txt");
         System.out.println(data);
 
 
