1be7337ba9e8a56339b94aab43256992a5f4484f
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-05-23 17:39:24 +0200

    Registration endpoint

---
+++ server/src/main/java/com/inHealth/server/service/UserService.java
@@ -0,0 +1,17 @@
+package com.inHealth.server.service;
+
+import com.inHealth.server.model.User;
+import com.inHealth.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
