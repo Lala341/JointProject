3f3b99b1db773307e0d6ed0f3e46a70bd43a2f52
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-05-25 18:35:32 +0200

    Moved generating KPIs to file upload

--- server/src/main/java/com/inHealth/server/service/BiometricService.java
+++ server/src/main/java/com/inHealth/server/service/BiometricService.java
@@ -1,12 +1,19 @@
 package com.inHealth.server.service;
 
+import com.inHealth.server.model.DistanceKPI;
+import com.inHealth.server.model.StepsKPI;
 import com.inHealth.server.repository.BiometricRepository;
+import com.inHealth.server.repository.DistanceKPIRepository;
+import com.inHealth.server.repository.StepsKPIRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import static java.lang.Math.min;
 
 
 @Service
@@ -15,7 +22,28 @@ public class BiometricService {
     @Autowired
     private BiometricRepository biometricRepository;
 
+    @Autowired
+    private DescriptiveAnalysisService descriptiveAnalysisService;
+
+    @Autowired
+    private DistanceKPIRepository distanceKPIRepository;
+
+    @Autowired
+    private StepsKPIRepository stepsKPIRepository;
+
     public void uploadToHdfs(String content, String name) throws IOException {
-        biometricRepository.uploadToHdfs(content, name);
+        String user = name.split("_")[min(1,name.split("_").length)];
+        String fileName = name+".txt";
+        // Calculate total distance
+        biometricRepository.uploadToHdfs(content, user, fileName);
+        double totalDistance = descriptiveAnalysisService.calculateDistance(user, fileName);
+        // Store the calculated KPI in MongoDB
+        DistanceKPI distanceKPI = new DistanceKPI(null, user, LocalDateTime.now(), totalDistance);
+        distanceKPIRepository.save(distanceKPI);
+        // Calculate total steps
+        int totalSteps = descriptiveAnalysisService.calculateTotalSteps(user, fileName, 1.5, 50, 25);
+        // Store the calculated KPI in MongoDB
+        StepsKPI stepsKPI = new StepsKPI(null, user, LocalDateTime.now(), totalSteps);
+        stepsKPIRepository.save(stepsKPI);
     }
 }
