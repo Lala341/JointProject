538b88b278987bb3f961bb7efa1e12b2439a2415
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-05-25 20:49:38 +0200

    Basic mongodb querying

--- server/src/main/java/com/inHealth/server/repository/DistanceKPIRepository.java
+++ server/src/main/java/com/inHealth/server/repository/DistanceKPIRepository.java
@@ -2,8 +2,13 @@ package com.inHealth.server.repository;
 
 import com.inHealth.server.model.DistanceKPI;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DistanceKPIRepository extends MongoRepository<DistanceKPI, String> {
+    List<DistanceKPI> findByUserAndDateBetween(String user, LocalDate startDate, LocalDate endDate);
 }
